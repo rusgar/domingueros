4333289fad10267818bbc7d44f533c7ce50181fd
rusgar <edamamon@gmail.com>
2020-12-28 12:13:50 +0100

    Pagina de inicio

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const handlebars = require("express-handlebars");
+
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("inicio", {layout: "principal"});
+});
+
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
+
+
